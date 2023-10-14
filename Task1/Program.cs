db2f8905d041201e5ec5c7e350b1e7e87440204a
zhyk <zhyk@list.ru>
2023-10-14 20:22:36 +0300

    Задача № 1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача на вход принимает два числа и выводит, какое число большее, а какое меньшее.
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+    {
+        Console.WriteLine($"Первое число {number1} больше второго числа {number2}");
+    }
+else 
+    {
+        if (number2 > number1)
+        {
+            Console.WriteLine($"Второе число {number2} больше первого числа {number1}");
+        }
+        else
+        {
+            Console.WriteLine("Числа равны");
+        }
+    }
