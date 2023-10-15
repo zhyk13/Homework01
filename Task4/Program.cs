da2e306a0f4e6f97786bf9566a2b4a5c08a1f527
zhyk <zhyk@list.ru>
2023-10-15 10:43:34 +0300

    Задача № 4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿//На вход принимает число (number), а на выходе выводит все чётные
+//числа от 1 до number (включительно), разделеные знаком табуляции.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while (index <= number)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write(index);
+        Console.Write("\t");
+    }
+    index++;    
+}
